98da98412412eb52d87c580d23bd2fafabcd010f
ainem <77418538+ainem-m@users.noreply.github.com>
2023-01-05 08:22:10 +0900

    add comment

--- make_qrcode.py
+++ make_qrcode.py
@@ -1,6 +1,11 @@
 import qrcode
 import sys
-text = input()
+
+# 使い方
+# python3 make_qrcode.py >hoge.jpg
+# のようにして出力をリダイレクトして使う
+# TODO: シェルで引数を取れるようにしたい
+text = input("text: ")
 img = qrcode.make(text)
 
 img.save(sys.stdout)
