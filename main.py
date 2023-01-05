60aced76f4dc5f363d3baf1c626cfc2ff974d741
ainem-m <ainemmikkeler@gmail.com>
2023-01-05 17:45:21 +0900

    ロジックの整理、設定を別ファイルに切り分け

--- main.py
+++ main.py
@@ -1,20 +1,11 @@
 from datetime import datetime
-import os
 import subprocess
 from typing import List, Tuple, Optional
 import shutil
 import sys
 from pathlib import Path
 import decode
-# ---
-# 設定
-
-# 振り分ける対象のフォルダ
-STR_ROOT_DIR: str = "/Users/ainem/Desktop/python/photo_manage/sample/"
-# 振り分け先のフォルダ
-STR_TARGET_DIR: str = "/Users/ainem/Desktop/python/photo_manage/sample2/"
-
-# ---
+import settings
 
 
 def get_date(path: Path) -> datetime:
@@ -25,9 +16,20 @@ def date_to_str(date: datetime) -> str:
     return f"{date.year}-{date.month:02d}-{date.day:02d}"
 
 
+def make_filepath(dst: Path, date: datetime, cnt: int) -> Tuple[Path, int]:
+    path = dst / f"{date_to_str(date)}-{cnt:02d}.jpg"
+    if path.exists():
+        print("you may run this program twice:", file=sys.stderr)
+        print(f"{path} is already exists", file=sys.stderr)
+        while path.exists():
+            cnt += 1
+            path = path.parent / f"{date_to_str(date)}-{cnt:02d}.jpg"
+    return path, cnt
+
+
 def main():
-    ROOT_DIR: Path = Path(STR_ROOT_DIR)
-    TARGET_DIR: Path = Path(STR_TARGET_DIR)
+    ROOT_DIR: Path = Path(settings.ROOT_DIR)
+    TARGET_DIR: Path = Path(settings.TARGET_DIR)
     assert ROOT_DIR.exists() and TARGET_DIR.exists()
 
     (TARGET_DIR / "qr").mkdir(exist_ok=True)
@@ -45,47 +47,32 @@ def main():
     current_time = sorted_filelist[0][1]
     target_dir: str = TARGET_DIR
     current_day: datetime.day = current_time.day
-    file_number: int = 0
+    cnt: int = 0
 
     for i, (path, date) in enumerate(sorted_filelist):
         folder_name: Optional[str] = decode.decode(path)
-        # QRCodeが見つかったらフォルダを作成
-        if folder_name or i == 0 or date.day != current_day:
+        if folder_name:
             current_day = date.day
-            if folder_name:
-                target_dir = TARGET_DIR / folder_name
-                # QRCodeの画像は避難
-                qr_path: Path = TARGET_DIR / "qr" / \
-                    f"{date_to_str(date)}-{file_number:02d}.jpg"
-                while qr_path.exists():
-                    print(
-                        f"you may run this program twice: \n{qr_path} is already exists", file=sys.stderr)
-                    file_number += 1
-                    qr_path = qr_path.parent / \
-                        f"{date_to_str(date)}-{file_number:02d}.jpg"
-
-                shutil.copy(src=path, dst=qr_path)
-                file_number += 1
-                continue
-            else:
-                # QRCodeが見つからずに日付が変わった場合
-                # おそらくQRCodeの撮り忘れなので、日付の名前のフォルダを作成
-                target_dir = TARGET_DIR / (date_to_str(date) + "unknown")
+            # QRCodeが見つかったらフォルダを作成
+            target_dir = TARGET_DIR / folder_name
+            target_dir.mkdir(exist_ok=True)
+            # QRCodeの画像は避難
+            qr_path, cnt = make_filepath(TARGET_DIR / "qr", date, cnt)
+            shutil.copy(src=path, dst=qr_path)
+            cnt += 1
+            continue
+        if i == 0 or date.day != current_day:
+            current_day = date.day
+            # QRCodeが見つからずに日付が変わった場合
+            # おそらくQRCodeの撮り忘れなので、日付の名前のフォルダを作成
+            target_dir = TARGET_DIR / (date_to_str(date) + "unknown")
 
         target_dir.mkdir(exist_ok=True)
 
-        img_path: Path = target_dir / \
-            f"{date_to_str(date)}-{file_number:02d}.jpg"
-
-        while img_path.exists():
-            print(
-                f"you may run this program twice:\n{img_path} is already exists", file=sys.stderr)
-            file_number += 1
-            img_path = img_path.parent / \
-                f"{date_to_str(date)}-{file_number:02d}.jpg"
+        img_path, cnt = make_filepath(target_dir, date, cnt)
         shutil.copy(src=path, dst=img_path)
-        file_number += 1
-    os.chdir(TARGET_DIR)
+        cnt += 1
+    subprocess.run(f"chdir {TARGET_DIR}")
     res = subprocess.run("tree", capture_output=True, text=True)
     print(res.stdout, file=sys.stderr)
 
---
+++ settings.py
@@ -0,0 +1,9 @@
+# ---
+# 設定
+
+# 振り分ける対象のフォルダ
+ROOT_DIR: str = "/Users/ainem/Desktop/python/photo_manage/sample/"
+# 振り分け先のフォルダ
+TARGET_DIR: str = "/Users/ainem/Desktop/python/photo_manage/sample2/"
+
+# ---
